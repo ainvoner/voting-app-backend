7b6f804b0fd1936f933a5b04264cf79c4947d08a
Ainvoner <shaia@monada.co>
2024-08-04 15:20:49 +0300

    wip

--- scripts/build-apply-all.sh
+++ scripts/build-apply-all.sh
@@ -4,25 +4,25 @@
 
 echo "Building and pushing BE"
 
-docker build -t localhost:5001/voting-app-backend:latest .
+docker build -t kind-registry:5001/voting-app-backend:latest .
 
-docker push localhost:5001/voting-app-backend:latest
+docker push kind-registry:5001/voting-app-backend:latest
 
 # Cast A Vote App
 
 echo "Building and pushing Cast A Vote App"
 
-docker build -t localhost:5001/vote-casting-app:latest ../vote-casting-app/
+docker build -t kind-registry:5001/vote-casting-app:latest ../vote-casting-app/
 
-docker push localhost:5001/vote-casting-app:latest
+docker push kind-registry:5001/vote-casting-app:latest
 
 # Votes results App
 
 echo "Building and pushing Votes results App"
 
-docker build -t localhost:5001/votes-results-app:latest ../votes-results-app/
+docker build -t kind-registry:5001/votes-results-app:latest ../votes-results-app/
 
-docker push localhost:5001/votes-results-app:latest
+docker push kind-registry:5001/votes-results-app:latest
 
 # Apply to local cluster
 
